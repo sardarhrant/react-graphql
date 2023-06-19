c622678a8ef2f6fcd4ced1d956333c75de222d5d
Hrant Sardaryan <hrant_sardaryan@epam.com>
2023-06-19 17:46:01 +0400

    Added auth token in db.json

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const github = {
+    baseURL: "https://api.github.com/graphql",
+    username: "sardaryanhrant",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: "bearer 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"
+    }
+};
+
+export default github;
